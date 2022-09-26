4b200ac7602d407fff86129f3f4d1a931d89ea10
Olle <ollny930@student.liu.se>
2022-09-26 15:54:03 +0200

    added test to check if cu.traj is created

--- inttest_md.py
+++ inttest_md.py
@@ -1,14 +1,21 @@
 import md
 import sys, unittest
-import pathlib as pl
+import os
 
-md.run_md(md)
+md.run_md()
 
 class MdTests(unittest.TestCase):
 
-    def assertIsFile(self, path):
-        def test(self):
-            path = pl.Path("cu.traj")
-            self.assertIsFile(path)
+    def test_calcenergy(self):
+        path = "/Users/olle/Documents/TFYA99/project-hands-on/hands-on-3/cu.traj"
+        isFile = os.path.isfile(path)
+        self.assertTrue(isFile)
+
+if __name__ == "__main__":
+    tests = [unittest.TestLoader().loadTestsFromTestCase(MdTests)]
+    testsuite = unittest.TestSuite(tests)
+    result = unittest.TextTestRunner(verbosity=0).run(testsuite)
+    sys.exit(not result.wasSuccessful())
+
 
 
