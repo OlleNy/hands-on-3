f0498afdb7bdce59124b42fbca6946570dc681fb
Olle <ollny930@student.liu.se>
2022-09-26 14:11:47 +0200

    added unit test of function calcenergy and restructured md.py

---
+++ unittest_md.py
@@ -0,0 +1,36 @@
+import sys, unittest
+from md import calcenergy
+from ase.lattice.cubic import FaceCenteredCubic
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+
+# Use Asap for a huge performance increase if it is installed
+use_asap = True
+
+if use_asap:
+    from asap3 import EMT
+    size = 10
+else:
+    from ase.calculators.emt import EMT
+    size = 3
+
+# Set up a crystal
+atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                          symbol="Cu",
+                          size=(size, size, size),
+                          pbc=True)
+
+atoms.calc = EMT()
+
+MaxwellBoltzmannDistribution(atoms, temperature_K=300)
+
+class MdTests(unittest.TestCase):
+
+    def test_calcenergy(self):
+        epot, ekin, inst_temp, etot = calcenergy(atoms)
+        self.assertEqual(round(etot, 3), 0.038)
+
+if __name__ == "__main__":
+    tests = [unittest.TestLoader().loadTestsFromTestCase(MdTests)]
+    testsuite = unittest.TestSuite(tests)
+    result = unittest.TextTestRunner(verbosity=0).run(testsuite)
+    sys.exit(not result.wasSuccessful())
